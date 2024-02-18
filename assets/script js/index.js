d1461b18bf2a9f8767bb9a1df83e837da14cfda3
shahinexy <shahinexy@gmail.com>
2024-02-18 14:32:18 +0600

    total and grand total price calculation done

--- assets/script js/index.js
+++ assets/script js/index.js
@@ -1,5 +1,8 @@
 let seatCount = 0;
 let seatLeft = 40;
+let totalPrice = 0;
+// let grandTotal = totalPrice;
+
 const allSeat = document.querySelectorAll('.seat_btn');
 
 for(const seat of allSeat){
@@ -15,5 +18,25 @@ for(const seat of allSeat){
         setInnerText('seat_count', seatCount)
         seatLeft -= 1;
         setInnerText('seat_left', seatLeft);
+
+        // seat details added
+        const seatDetails = document.getElementById('seat_details');
+        const p = document.createElement('p');
+        p.innerText = e.target.innerText;
+        const p2 = document.createElement('p');
+        p2.innerText = 'Economoy';
+        const p3 = document.createElement('p');
+        p3.innerText = '550';
+        p3.style.textAlign = 'end';
+        seatDetails.append(p);
+        seatDetails.append(p2);
+        seatDetails.append(p3);
+
+        // added total & grand total price
+        totalPrice += 550;
+        setInnerText('total_price', totalPrice);
+        grandTotal = totalPrice;
+        setInnerText('grand_total', grandTotal);
+        console.log(grandTotal);
     })
 }
